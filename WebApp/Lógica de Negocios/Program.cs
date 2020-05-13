b4600f0563a4547bec7f8a65389a6967514659aa
EMANUEL\emazo <emanuelzoja@gmail.com>
2020-05-13 18:11:47 -0300

    Logica de negocios y servicios

---
+++ WebApp/Lógica de Negocios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lógica_de_Negocios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
