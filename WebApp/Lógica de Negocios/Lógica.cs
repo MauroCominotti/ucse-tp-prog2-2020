030407af10ce9bdbf975f71509bf19966895c191
MauroCominotti <maurocominotti@hotmail.com>
2020-05-13 18:26:36 -0300

    Logica con metodos de Interfaz

--- WebApp/Lógica de Negocios/Lógica.cs
+++ WebApp/Lógica de Negocios/Lógica.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contratos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Lógica_de_Negocios
 {
-    public sealed class Lógica
+    public sealed class Lógica : IServicioWeb
     {
         private static Lógica instance = null;
         private Lógica()
@@ -23,6 +24,171 @@ namespace Lógica_de_Negocios
                 }
                 return instance;
             }
+        }
+
+        public Resultado AltaAlumno(Hijo hijo, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado AltaDirectora(Directora directora, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado AltaDocente(Docente docente, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado AltaNota(Nota nota, Sala[] salas, Hijo[] hijos, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado AltaPadreMadre(Padre padre, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado AsignarDocenteSala(Docente docente, Sala sala, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado AsignarHijoPadre(Hijo hijo, Padre padre, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado DesasignarDocenteSala(Docente docente, Sala sala, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado DesasignarHijoPadre(Hijo hijo, Padre padre, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EditarAlumno(int id, Hijo hijo, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EditarDirectora(int id, Directora directora, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EditarDocente(int id, Docente docente, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EditarPadreMadre(int id, Padre padre, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EliminarAlumno(int id, Hijo hijo, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EliminarDirectora(int id, Directora directora, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EliminarDocente(int id, Docente docente, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado EliminarPadreMadre(int id, Padre padre, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado MarcarNotaComoLeida(Nota nota, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hijo ObtenerAlumnoPorId(UsuarioLogueado usuarioLogueado, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Grilla<Hijo> ObtenerAlumnos(UsuarioLogueado usuarioLogueado, int paginaActual, int totalPorPagina, string busquedaGlobal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Nota[] ObtenerCuadernoComunicaciones(int idPersona, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Directora ObtenerDirectoraPorId(UsuarioLogueado usuarioLogueado, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Grilla<Directora> ObtenerDirectoras(UsuarioLogueado usuarioLogueado, int paginaActual, int totalPorPagina, string busquedaGlobal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Docente ObtenerDocentePorId(UsuarioLogueado usuarioLogueado, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Grilla<Docente> ObtenerDocentes(UsuarioLogueado usuarioLogueado, int paginaActual, int totalPorPagina, string busquedaGlobal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Institucion[] ObtenerInstituciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ObtenerNombreGrupo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Padre ObtenerPadrePorId(UsuarioLogueado usuarioLogueado, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Grilla<Padre> ObtenerPadres(UsuarioLogueado usuarioLogueado, int paginaActual, int totalPorPagina, string busquedaGlobal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hijo[] ObtenerPersonas(UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sala[] ObtenerSalasPorInstitucion(UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UsuarioLogueado ObtenerUsuario(string email, string clave)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Resultado ResponderNota(Nota nota, Comentario nuevoComentario, UsuarioLogueado usuarioLogueado)
+        {
+            throw new NotImplementedException();
         }
     }
 }
